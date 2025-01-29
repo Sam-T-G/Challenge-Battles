7a8082c10cfb3d6a9ac54838e4f7e65023e4b3b3
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2025-01-29 12:34:40 -0800

    Create game.cpp

---
+++ game.cpp
@@ -0,0 +1,45 @@
+/*
+Name: Samuel gerungan
+Date: 1/28/25
+Purpose: Create a game based off of basic programming techniques
+!!GAME CANNOT UTILIZE FUNCTIONS OR ARRAYS!!
+
+**Main Viable Product Description**
+
+*Game will preset three battle challenges that consist of hardcoded pets that will have a predetermined element and moveset*
+
+-Elemental Advantages-
+Fire > Grass > Water > Fire ...
+Attacks against advantageous elemental property yield 25% more damage
+Attacks against detrimental elemental property yield 25% less damage
+
+*/
+
+// System Libraries
+#include <iostream>
+using namespace std;
+
+// User Libraries
+
+// Global Constants
+
+// Only use scientific values like pi, speed of light, etc
+
+// Conversion between units
+
+// Function Protypes
+int main(int argv, char **argc)
+{
+    // Random seeds are set for random number generator
+
+    // Declare Variables
+
+    // Initialize Variables
+
+    // Map the inputs and outputs - Process
+
+    // Display and output the results
+
+    // Exit the program
+    return 0;
+}
